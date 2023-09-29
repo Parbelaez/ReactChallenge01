09e187fe60368a69c25c9d41d2ccb8258b2a9c28
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-09-29 10:08:17 +0000

    Add the links to the Sidebar func.

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,10 +1,5 @@
 const Sidebar = () => {
-    return
-            <div>
-                <a></a>
-                <a></a>
-                <a></a>
-            </div>
+    return <div><a>My Photos</a><a>My Illustrations</a><a>My Paintings</a></div>
 };
 
 export default Sidebar;
