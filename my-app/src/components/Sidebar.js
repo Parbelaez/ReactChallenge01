b4cb9810e3aeebd19d631c9d798fae30de0d8d90
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-09-29 21:08:10 +0000

    Add styling to Sidebar

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,5 +1,7 @@
+import css from "./css/Sidebar.module.css"
+
 const Sidebar = () => {
-    return <div><a>My Photos</a><a>My Illustrations</a><a>My Paintings</a></div>
+    return <div className = {css.sidebar}><a>My Photos</a><a>My Illustrations</a><a>My Paintings</a></div>
 };
 
 export default Sidebar;
